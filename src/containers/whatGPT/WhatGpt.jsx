ac3ce646494704dd487486cc246573bfd1d509a1
rabby-shek <rabbyshek1998@gmail.com>
2024-05-17 01:28:23 +0600

    what gpt component view bug fixed

--- src/containers/whatGPT/WhatGpt.jsx
+++ src/containers/whatGPT/WhatGpt.jsx
@@ -14,7 +14,6 @@ const WhatGpt = () => {
         <h1 className="gradient__text">
           The possibilities are beyond your imagination
         </h1>
-        <p>Explore the Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
         <Feature
