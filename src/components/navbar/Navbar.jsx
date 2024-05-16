638589edb291cd15fffb81586ad43770ec6423d9
rabby-shek <rabbyshek1998@gmail.com>
2024-05-17 00:45:21 +0600

    responsive navbar bug fixed and complete

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,49 +1,90 @@
-import React, {useState} from 'react'
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
-import './navbar.css';
+import "./navbar.css";
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
+  console.log(toggleMenu);
   return (
     <div className="gpt__navbar">
-    <div className="gpt__navbar-links">
-      <div className="gpt__navbar-links_logo">
-        <img src={logo} />
-      </div>
-      <div className="gpt__navbar-links_container">
-        <p><a href="#home">Home</a></p>
-        <p><a href="#wgpt">What is gpt?</a></p>
-        <p><a href="#possibility">Open AI</a></p>
-        <p><a href="#features">Case Studies</a></p>
-        <p><a href="#blog">Library</a></p>
-      </div>
-    </div>
-    <div className="gpt__navbar-sign">
-      <p>Sign in</p>
-      <button type="button">Sign up</button>
-    </div>
-    <div className="gpt__navbar-menu">
-      {toggleMenu
-        ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
-        : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
-      {toggleMenu && (
-      <div className="gpt__navbar-menu_container scale-up-center">
-        <div className="gpt__navbar-menu_container-links">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt">What is gpt?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+      <div className="gpt__navbar-links">
+        <div className="gpt__navbar-links_logo">
+          <img src={logo} />
         </div>
-        <div className="gpt__navbar-menu_container-links-sign">
-          <p>Sign in</p>
-          <button type="button">Sign up</button>
+        <div className="gpt__navbar-links_container">
+          <p>
+            <a href="#home">
+              Home
+            </a>
+          </p>
+          <p>
+            <a href="#wgpt">
+              What is gpt?
+            </a>
+          </p>
+          <p>
+            <a href="#possibility" >
+              Open AI
+            </a>
+          </p>
+          <p>
+            <a href="#features" >
+              Case Studies
+            </a>
+          </p>
+          <p>
+            <a href="#blog">
+              Library
+            </a>
+          </p>
         </div>
       </div>
-      )}
+      <div className="gpt__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt__navbar-menu_container scale-up-center">
+            <div className="gpt__navbar-menu_container-links">
+              <p>
+                <a href="#home" onClick={() => setToggleMenu(false)}>Home</a>
+              </p>
+              <p>
+                <a href="#wgpt" onClick={() => setToggleMenu(false)}>What is gpt?</a>
+              </p>
+              <p>
+                <a href="#possibility" onClick={() => setToggleMenu(false)}>Open AI</a>
+              </p>
+              <p>
+                <a href="#features" onClick={() => setToggleMenu(false)}>Case Studies</a>
+              </p>
+              <p>
+                <a href="#blog" onClick={() => setToggleMenu(false)}>Library</a>
+              </p>
+            </div>
+            <div className="gpt__navbar-menu_container-links-sign">
+              <p>Sign in</p>
+              <button type="button">Sign up</button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default Navbar;
