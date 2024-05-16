ac4649b908bc4d6c9b6f1884df9a1d8ec9e82249
rabby-shek <rabbyshek1998@gmail.com>
2024-05-16 22:46:34 +0600

    app structure created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGpt,
+  Header,
+} from "./containers";
+import { Brand, Navbar, CTA } from "./components";
 const App = () => {
   return (
-    <div>
-      app
+    <div className="app">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGpt />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/containers/whatGPT/WhatGpt.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WhatGpt = () => {
+  return (
+    <div>
+      WhatGpt
+    </div>
+  )
+}
+
+export default WhatGpt
