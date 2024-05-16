3530bba1d6a12c6a47302c156a6de2f69dcd64d1
rabby-shek <rabbyshek1998@gmail.com>
2024-05-17 01:26:27 +0600

    feature component documentation complete

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
-import './feature.css';
-
+import React from "react";
+import "./feature.css";
+/**
+ *
+ * @param {title} param1 //feature title
+ * @param {text} param2 //text for feature
+ * @component
+ * @returns {JSX.Element} => A ReactJs Component
+ */
 const Feature = ({ title, text }) => (
   <div className="gpt3__features-container__feature">
     <div className="gpt3__features-container__feature-title">
